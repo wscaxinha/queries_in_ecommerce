505e20fa1554d282c4015663bf62c5effe67e458
Washington Alves <www.alves.silva@gmail.com>
2024-11-22 01:47:27 -0300

    Adicionando Readme junto com script E-commerce

---
+++ README.md
@@ -0,0 +1,120 @@
+# 📈 E-Commerce Database Queries
+<p style="text-align: justify;">
+Este repositório contém um conjunto de queries SQL para um sistema de E-commerce, aplicando diversos conceitos e cláusulas SQL para gerar relatórios e analisar dados do banco. Abaixo estão as cláusula implementada nas consultas.
+</p>
+
+## 1. Atributo Derivado - Status de Rastreamento
+<p style="text-align: justify;">
+<strong style="color: #FFA500;">Objetivo : </strong> Gerar um relatório com o status de rastreio de cada pedido, classificando-os conforme o status de entrega, baseado nas informações da tabela **Rastreio**.
+</p>
+
+```sql
+SELECT 
+    Pedido.idPedido,
+    CASE
+        WHEN Rastreamento.numero_rastreio IS NOT NULL AND Rastreamento.status = 'Em Transito' THEN 'Em Transito'
+        WHEN Rastreamento.numero_rastreio IS NOT NULL AND Rastreamento.status = 'Entregue' THEN 'Entregue'
+        ELSE 'Não Iniciado'
+    END AS status_rastreio
+FROM Pedido
+LEFT JOIN Rastreamento ON Pedido.idPedido = Rastreamento.idPedido;
+```
+
+## 2. Cálculo do Valor Total de Pedidos
+<p style="text-align: justify;">
+<strong style="color: #FFA500;">Objetivo : </strong> Calcular o valor total de cada pedido realizado pelos clientes, multiplicando a quantidade de cada item pelo seu preço e somando esses valores para cada pedido.
+</p>
+
+```sql
+SELECT Pedido.idPedido, Cliente.nome, SUM(Pedido_Produto.quantidade * Pedido_Produto.preco) AS valor_total_calculado
+FROM Pedido_Produto
+INNER JOIN Pedido ON Pedido_Produto.idPedido = Pedido.idPedido
+INNER JOIN Cliente ON Pedido.idCliente = Cliente.idCliente
+GROUP BY Pedido.idPedido, Cliente.nome;
+```
+## 3. Total de Pedidos por Cliente
+<p style="text-align: justify;">
+<strong style="color: #FFA500;">Objetivo : </strong> Exibir cada cliente e o total de pedidos feitos por ele, ordenando em ordem decrescente pelo número de pedidos realizados.
+</p>
+
+```sql
+SELECT Cliente.idCliente, Cliente.nome AS NomeCliente, COUNT(Pedido.idPedido) AS TotalPedidos
+FROM Cliente INNER JOIN Pedido ON Cliente.idCliente = Pedido.idCliente
+GROUP BY Cliente.idCliente, Cliente.nome ORDER BY TotalPedidos DESC;
+```
+## 4. Fornecedores e Produtos
+<p style="text-align: justify;">
+<strong style="color: #FFA500;">Objetivo : </strong> Listar os fornecedores e os produtos que eles fornecem, considerando a relação entre as tabelas Fornecedor e Produto.
+</p>
+
+```sql
+SELECT Fornecedor.nome_empresa AS NomeFornecedor, Produto.Nome AS NomeProduto
+FROM Fornecedor
+INNER JOIN Fornecimento ON Fornecedor.idFornecedor = Fornecimento.idFornecedor
+INNER JOIN Produto ON Fornecimento.idProduto = Produto.idProduto
+LIMIT 0, 1000;
+```
+## 5. Produtos, Fornecedores e Estoque
+<p style="text-align: justify;">
+<strong style="color: #FFA500;">Objetivo : </strong> Relacionar fornecedores, produtos e o estoque, exibindo informações sobre o produto, quantidade em estoque e sua localização.
+</p>
+
+```sql
+SELECT Fornecedor.nome_empresa AS NomeFornecedor, 
+       Produto.Nome AS NomeProduto, 
+       Estoque.quantidade AS QuantidadeEmEstoque, 
+       Estoque.localizacao AS LocalizacaoEstoque
+FROM Fornecedor
+INNER JOIN Fornecimento ON Fornecedor.idFornecedor = Fornecimento.idFornecedor
+INNER JOIN Produto ON Fornecimento.idProduto = Produto.idProduto
+INNER JOIN Estoque ON Produto.idProduto = Estoque.idProduto;
+```
+## 6. Clientes com Mais de 1 Pedido
+<strong style="color: #FFA500;">Objetivo : </strong> Retornar os clientes que realizaram mais de 1 pedido.
+
+```sql
+SELECT Cliente.idCliente, Cliente.nome, COUNT(Pedido.idPedido) AS TotalPedidos
+FROM Cliente
+INNER JOIN Pedido ON Cliente.idCliente = Pedido.idCliente
+GROUP BY Cliente.idCliente, Cliente.nome
+HAVING COUNT(Pedido.idPedido) > 1;
+```
+## 7. Fornecedores com Valor Total Superior a R$ 200.000,00
+<strong style="color: #FFA500;">Objetivo : </strong> Retornar os fornecedores cujos produtos fornecidos tenham um valor total superior a R$ 200.000,00.
+
+```sql
+SELECT Fornecedor.nome_empresa AS NomeFornecedor, 
+       SUM(Pedido_Produto.quantidade * Pedido_Produto.preco) AS ValorTotalFornecido
+FROM Fornecedor
+INNER JOIN Fornecimento ON Fornecedor.idFornecedor = Fornecimento.idFornecedor
+INNER JOIN Produto ON Fornecimento.idProduto = Produto.idProduto
+INNER JOIN Pedido_Produto ON Produto.idProduto = Pedido_Produto.idProduto
+GROUP BY Fornecedor.idFornecedor, Fornecedor.nome_empresa
+HAVING SUM(Pedido_Produto.quantidade * Pedido_Produto.preco) > 200000;
+```
+## 8. Desempenho dos Vendedores
+<p style="text-align: justify;">
+<strong style="color: #FFA500;">Objetivo : </strong> Relacionar vendedores com os pedidos que processaram, calculando o valor total dos pedidos e listando os produtos vendidos.
+</p>
+
+```sql
+SELECT 
+    Vendedor.idVendedor,
+    Vendedor.nome AS NomeVendedor,
+    Pedido.idPedido,
+    SUM(Pedido_Produto.quantidade * Pedido_Produto.preco) AS ValorTotalPedido,
+    GROUP_CONCAT(DISTINCT Produto.Nome ORDER BY Produto.Nome) AS ProdutosVendidos
+FROM Pedido
+INNER JOIN Vendedor ON Pedido.idVendedor = Vendedor.idVendedor
+INNER JOIN Pedido_Produto ON Pedido.idPedido = Pedido_Produto.idPedido
+INNER JOIN Produto ON Pedido_Produto.idProduto = Produto.idProduto
+GROUP BY Vendedor.idVendedor, Pedido.idPedido
+ORDER BY ValorTotalPedido DESC;
+```
+## Conclusão
+<p style="text-align: justify;">
+As queries implementadas no banco de dados do E-commerce abordam uma variedade de operações SQL, incluindo junções entre múltiplas tabelas, funções de agregação, e filtros condicionais. Elas fornecem uma visão abrangente sobre o comportamento dos clientes, fornecedores, produtos, e vendedores, e são essenciais para análise e relatórios em sistemas de E-commerce.
+</p>
+
+
+
---
+++ Script_Ecommerce.sql
@@ -0,0 +1,173 @@
+CREATE DATABASE Ecommerce;
+USE Ecommerce;
+
+-- Tabela Fornecedor
+CREATE TABLE Fornecedor (
+    idFornecedor INT NOT NULL AUTO_INCREMENT,
+    nome_empresa VARCHAR(50) NOT NULL,
+    cnpj VARCHAR(14) NOT NULL,
+    PRIMARY KEY (idFornecedor),
+    CONSTRAINT chk_cnpj CHECK (LENGTH(cnpj) = 14 AND cnpj REGEXP '^[0-9]+$')
+);
+
+-- Tabela Produto
+CREATE TABLE Produto (
+    idProduto INT NOT NULL AUTO_INCREMENT,
+    Nome VARCHAR(50) NOT NULL,
+    Descricao VARCHAR(100),
+    Preco DECIMAL(10, 2) NOT NULL,
+    Categoria VARCHAR(50) NOT NULL,
+    PRIMARY KEY (idProduto),
+    CONSTRAINT chk_preco_produto CHECK (Preco > 0)
+);
+
+-- Tabela de Fornecimento
+CREATE TABLE Fornecimento (
+    idFornecedor INT NOT NULL,
+    idProduto INT NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidade INT NOT NULL,
+    PRIMARY KEY (idFornecedor, idProduto),
+    CONSTRAINT chk_preco_fornecimento CHECK (preco > 0),
+    CONSTRAINT chk_quantidade_fornecimento CHECK (quantidade >= 0),
+    CONSTRAINT fk_fornecimento_fornecedor FOREIGN KEY (idFornecedor) REFERENCES Fornecedor(idFornecedor) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_fornecimento_produto FOREIGN KEY (idProduto) REFERENCES Produto(idProduto) 
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela Estoque
+CREATE TABLE Estoque (
+    idProduto INT NOT NULL,
+    quantidade INT NOT NULL,
+    localizacao VARCHAR(50),
+    dataUltimaEntrada DATE,
+    dataUltimaSaida DATE,
+    nivelMinimo INT NOT NULL,
+    custoUnitario DECIMAL(10, 2) NOT NULL,
+    observacoes TEXT,
+    PRIMARY KEY (idProduto),
+    CONSTRAINT chk_quantidade_estoque CHECK (quantidade >= 0),
+    CONSTRAINT chk_nivel_minimo CHECK (nivelMinimo >= 0),
+    CONSTRAINT fk_estoque_produto FOREIGN KEY (idProduto) REFERENCES Produto(idProduto) 
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela TipoCliente
+CREATE TABLE TipoCliente (
+    idTipoCliente INT NOT NULL AUTO_INCREMENT,
+    descricao_cliente ENUM('PF', 'PJ') NOT NULL,
+    PRIMARY KEY (idTipoCliente)
+);
+
+-- Tabela Cliente
+CREATE TABLE Cliente (
+    idCliente INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telefone VARCHAR(20),
+    endereco VARCHAR(100),
+    cpf VARCHAR(11),
+    cnpj VARCHAR(14),
+    idTipoCliente INT NULL,
+    PRIMARY KEY (idCliente),
+    CONSTRAINT chk_cpf_cliente CHECK (cpf IS NULL OR LENGTH(cpf) = 11 AND cpf REGEXP '^[0-9]+$'),
+    CONSTRAINT chk_cnpj_cliente CHECK (cnpj IS NULL OR LENGTH(cnpj) = 14 AND cnpj REGEXP '^[0-9]+$'),
+    CONSTRAINT fk_cliente_tipocliente FOREIGN KEY (idTipoCliente) REFERENCES TipoCliente(idTipoCliente) 
+        ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Tabela StatusPedido
+CREATE TABLE StatusPedido (
+    idStatusPedido INT NOT NULL AUTO_INCREMENT,
+    descricao ENUM('pendente', 'enviado', 'entregue', 'cancelado') NOT NULL,
+    PRIMARY KEY (idStatusPedido)
+);
+
+-- Tabela Rastreamento
+CREATE TABLE Rastreamento(
+    idPedido INT,
+    numero_rastreio VARCHAR(100),
+    status VARCHAR(50),
+    PRIMARY KEY (idPedido),
+    FOREIGN KEY (idPedido) REFERENCES Pedido(idPedido)
+);
+-- Tabela Vendedor
+CREATE TABLE Vendedor (
+    idVendedor INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,  
+    telefone VARCHAR(20),
+    PRIMARY KEY (idVendedor)
+);
+
+-- Tabela Pedido
+CREATE TABLE Pedido (
+    idPedido INT NOT NULL AUTO_INCREMENT,  
+    idCliente INT NULL, 
+    idStatusPedido INT NOT NULL,
+    idVendedor INT NOT NULL,
+    dataPedido DATE NOT NULL,
+    valorTotal DECIMAL(10, 2) NOT NULL,
+    codigoRastreio VARCHAR(50),
+    dataRastreio DATE,
+    ativo TINYINT DEFAULT 1, 
+    PRIMARY KEY (idPedido),
+    CONSTRAINT chk_valor_total CHECK (valorTotal > 0),
+    CONSTRAINT fk_pedido_cliente FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente) 
+        ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_pedido_status FOREIGN KEY (idStatusPedido) REFERENCES StatusPedido(idStatusPedido),
+    CONSTRAINT fk_pedido_vendedor FOREIGN KEY (idVendedor) REFERENCES Vendedor(idVendedor)
+);
+
+-- Tabela TipoPagamento
+CREATE TABLE TipoPagamento (
+    idTipoPagamento INT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(50) NOT NULL,
+    PRIMARY KEY (idTipoPagamento)
+);
+
+-- Tabela StatusPagamento
+CREATE TABLE StatusPagamento (
+    idStatusPagamento INT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(50) NOT NULL,
+    PRIMARY KEY (idStatusPagamento)
+);
+
+-- Tabela Pagamento
+CREATE TABLE Pagamento (
+    idPagamento INT NOT NULL AUTO_INCREMENT,
+    idPedido INT NULL,  
+    idTipoPagamento INT NOT NULL,
+    idStatusPagamento INT NOT NULL,
+    valorPago DECIMAL(10, 2) NOT NULL,
+    dataPagamento DATE NOT NULL,
+    ativo TINYINT DEFAULT 1, 
+    PRIMARY KEY (idPagamento),
+    CONSTRAINT chk_valor_pago CHECK (valorPago > 0),
+    CONSTRAINT fk_pagamento_pedido FOREIGN KEY (idPedido) REFERENCES Pedido(idPedido) 
+        ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_pagamento_tipopagamento FOREIGN KEY (idTipoPagamento) REFERENCES TipoPagamento(idTipoPagamento),
+    CONSTRAINT fk_pagamento_statuspagamento FOREIGN KEY (idStatusPagamento) REFERENCES StatusPagamento(idStatusPagamento)
+);
+
+-- Tabela Pedido_Produto (mapeamento entre Pedido e Produto)
+CREATE TABLE Pedido_Produto (
+    idPedido INT NOT NULL, 
+    idProduto INT NOT NULL, 
+    quantidade INT NOT NULL, 
+    preco DECIMAL(10, 2) NOT NULL, 
+    PRIMARY KEY (idPedido, idProduto), 
+    FOREIGN KEY (idPedido) REFERENCES Pedido(idPedido) ON DELETE CASCADE,
+    FOREIGN KEY (idProduto) REFERENCES Produto(idProduto) ON DELETE CASCADE
+);
+
+-- Índices adicionais
+CREATE INDEX idx_cliente_email ON Cliente(email);
+CREATE INDEX idx_produto_nome ON Produto(Nome);
+CREATE INDEX idx_pedido_cliente ON Pedido(idCliente);
+CREATE INDEX idx_pagamento_pedido ON Pagamento(idPedido);
+
+SHOW TABLES;
+
+
+
